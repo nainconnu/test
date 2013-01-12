4065f41160c56a9cb33a591038b1856f8772dec4
Hugues <huguesbonin@10.0.1.7>
2013-01-12 11:34:39 -0500

    Premier commit du projet

---
+++ src/test_project/Test_project.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test_project;
+
+/**
+ *
+ * @author huguesbonin
+ */
+public class Test_project {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
